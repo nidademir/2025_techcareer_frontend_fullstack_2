cac85078b41d4a561268b8f0dc5a239ffdbf982f
nidademir <nidademirqn@gmail.com>
2025-03-12 12:14:12 +0300

    revize

--- routers/blog_api_router.js
+++ routers/blog_api_router.js
@@ -1,51 +1,82 @@
 // Express Import
 const express = require("express");
 
+// Express Router
+const router = express.Router();
+
 // Express App
 const app = express();
 
-// Express Router
-const router = express.Router();
+//
+const fs = require("fs");
+
+//
+const path = require("path");
+
+// JSON-SERVER(DB) yolunu göstermek
+const dbFilePath = path.join(__dirname, "../db.json");
 
 /////////////////////////////////////////////////////////////////////////////////////
 
 // DRY (Don't Repeat Yourself) - Kendini Tekrar Etme
-const handleError = (err, response, message) => {
-  console.error("Error: ", err);
-  response.status(400).json({ message });
-}; //end handleError
+// Hata Yönetimi
+const handleError = (res, message) => {
+  res.status(400).json({ error: message });
+};
 
-//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+// JSON-SERVER(DB) VERI OKU
+const readDB = () => {
+  const data = fs.readFileSync(dbFilePath, "utf-8");
+  return JSON.parse(data);
+};
+
+/////////////////////////////////////////////////////////////////////////////////////
+// JSON-SERVER(DB) VERI YAZ
+const writeDB = (data) => {
+  fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2), "utf-8");
+};
+
+/////////////////////////////////////////////////////////////////////////////////////
 // BLOG API (CREATE)
-router.post("/", async (request, response) => {
-  // Data
-  const blogData = {
-    header: request.body.header,
-    content: request.body.content,
-    author: request.body.author,
-    tags: request.body.tags,
-  };
+router.post("/", (request, response) => {
   try {
-    // Save
-    const blog = await Blog.create(blogData);
-    response.status(200).json(blog);
+    // Verileri almak
+    const { header, content, author, tags } = request.body;
+    if (!header || !content || !author || !tags) {
+      return handleError(res, "Tüm alanlar gereklidir!");
+    }
 
-    // Ekleme Başarılı
-    console.warn("Blog Ekleme Başarılı", blog);
-  } catch (err) {
-    handleError(err, response, "Blog Ekleme Başarısız");
-  } //end catch
-}); //end router.post
+    // Dikkat: db.json içindeki dizi adı : blogs
+    // Veri tabanı
+    let db = readDB();
+    const newBlog = {
+      id: db.blogs.length ? db.blogs[db.blogs.length - 1].id + 1 : 1,
+      header,
+      content,
+      author,
+      tags,
+      createdAt: new Date().toISOString(),
+    };
+
+    db.blogs.push(newBlog);
+    writeDB(db);
+
+    res.status(201).json(newBlog);
+  } catch (error) {
+    handleError(res, "Blog ekleme işlemi başarısız!");
+  }
+});
 
 //////////////////////////////////////////////////////////////////////////////////////
 // BLOG API (LIST)
 router.get("/", async (request, response) => {
   try {
-    const blogs = await Blog.find({});
-    response.status(200).json(blogs);
+    const db = await readDB();
+    response.status(200).json(db.blogs);
 
     // Ekleme Başarılı
-    console.warn("Blog Listeme Başarılı", blogs);
+    console.warn("Blog Listeme Başarılı", "blogs");
   } catch (err) {
     handleError(err, response, "Blog Listeleme Başarısız");
   } //end catch
